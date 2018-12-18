3a08fb8b41760400be92c893bb3d26c1e20bcb62
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-18 15:29:15 +0100

    ex 07 tableaux listes enum

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabEx07
+{
+    class Program
+    {
+        enum week { Lundi, Mardi, Mercredi, Jeudi, Vendredi, Samedi, Dimanche }
+
+        static void Main(string[] args)
+        {
+            foreach (string allTheWeek in Enum.GetNames(typeof(week)))
+            {
+                Console.WriteLine(allTheWeek);
+            }
+            Console.WriteLine(Environment.NewLine + Enum.GetName(typeof(week), 4));
+
+        }
+    }
+}
